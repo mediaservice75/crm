a64605f787aca9dfe68b07dfa9b3f61646c3ff34
temso <solbonovartem@gmail.com>
2025-03-21 16:19:08 +0900

    update user sales stat table

--- resources/views/payment/paid-claims.blade.php
+++ resources/views/payment/paid-claims.blade.php
@@ -5,7 +5,7 @@
             <h3>Управление оплатами</h3>
         </div>
         <div class="col-12 col-md-6 text-end">
-            <a href="{{route('payment.paid')}}" class="btn btn-primary">Назад</a>
+            <a href="{{ route('payment.paid') }}" class="btn btn-primary">Назад</a>
         </div>
     </div>
 @endsection
@@ -18,41 +18,47 @@
                     <div class="card-body">
                         <h4 class="card-title mb-4 ">Список статусов оплаты</h4>
 
-                        @if($historiesPaidClaims->isEmpty())
+                        @if ($historiesPaidClaims->isEmpty())
                             <h5 class="text-gray-500">К сожалению, заявок не найдено 😢</h5>
                         @else
                             <table class="table table-lg table-hover" id="datatables">
                                 <thead>
-                                <tr>
-                                    <th>Заявка №</th>
-                                    <th>Дата</th>
-                                    <th>Статус</th>
-                                    <th>Сумма оплаты</th>
-                                    <th class="text-center">Удалить</th>
-                                </tr>
+                                    <tr>
+                                        <th>Заявка №</th>
+                                        <th>Дата</th>
+                                        {{-- <th>Номер счёта</th> --}}
+                                        <th>Статус</th>
+                                        <th>Сумма оплаты</th>
+                                        <th class="text-center">Удалить</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($historiesPaidClaims as $key => $item)
-                                    <tr>
-                                        <td>{{$item->claim_id}}</td>
-                                        <td>{{$item->getDate()}}</td>
-                                        <td>{{$item->status->name}}</td>
-                                        @if($item->status->name == 'Оплачен' || $item->status->name == 'Частично оплачен')
-                                            <td>{{money($item->amount)}} руб.</td>
-                                        @else
-                                            <td>-</td>
-                                        @endif
-                                        <td class="text-center">
-                                            <form action="{{route('payment.del-paid', ['id' => $item->id])}}" method="POST" class="d-inline">
-                                                @csrf
-                                                @method('DELETE')
-                                                <a href="#" class="text-danger ms-2 delete"><i class="bi bi-trash-fill"></i></a>
-                                            </form>
-                                        </td>
+                                    @foreach ($historiesPaidClaims as $key => $item)
+                                        <tr>
+                                            <td>{{ $item->claim_id }}</td>
+                                            <td>{{ $item->getDate() }}</td>
+                                            {{-- @dd($item)
+                                            @if ($item->number_invoice)
+                                                <td>{{ $item->number_invoice }}</td>
+                                            @endif --}}
+                                            <td>{{ $item->status->name }}</td>
+                                            @if ($item->status->name == 'Оплачен' || $item->status->name == 'Частично оплачен')
+                                                <td>{{ money($item->amount) }} руб.</td>
+                                            @else
+                                                <td>-</td>
+                                            @endif
+                                            <td class="text-center">
+                                                <form action="{{ route('payment.del-paid', ['id' => $item->id]) }}"
+                                                    method="POST" class="d-inline">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <a href="#" class="text-danger ms-2 delete"><i
+                                                            class="bi bi-trash-fill"></i></a>
+                                                </form>
+                                            </td>
 
-                                    </tr>
-
-                                @endforeach
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         @endif
